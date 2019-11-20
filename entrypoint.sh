554e77b81638bb2f15158c56b0b04be1e9d2a8bc
ambientlight <primary.taras.vozniuk@gmail.com>
2019-11-21 00:14:54 +0800

    should_skip_npm_install added

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,22 @@
 
 set -e
 
-if [ -z "$AWS_ACCESS_KEY_ID" ] && [ -z "$AWS_SECRET_ACCESS_KEY" ]
-then
+if [ -z "$AWS_ACCESS_KEY_ID" ] && [ -z "$AWS_SECRET_ACCESS_KEY" ] ; then
   echo "You must provide the action with both AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables in order to deploy"
   exit 1
 fi
 
-if [ -z "$AWS_DEFAULT_REGION" ]
-then
+if [ -z "$AWS_DEFAULT_REGION" ] ; then
   echo "You must provide AWS_DEFAULT_REGION in order to deploy"
 fi
 
-if [ -n "$1" ]
-then
+if [ -n "$1" ] ; then
   cd "$1"
 fi
 
-npm install
+if [ "$5" = false ] ; then
+  npm install
+fi
 
 echo "$2"
 echo "$3"
