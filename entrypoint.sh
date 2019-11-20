ff75e2f72f5713f7bc102438f5f0349220a182d8
ambientlight <primary.taras.vozniuk@gmail.com>
2019-11-20 21:51:52 +0800

    possibly fixed sh errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-if [ -z "$AWS_ACCESS_KEY_ID"] && [ -z "$AWS_SECRET_ACCESS_KEY" ]
+if [[ -z "$AWS_ACCESS_KEY_ID" ]] && [[ -z "$AWS_SECRET_ACCESS_KEY" ]]
 then
   echo "You must provide the action with both AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables in order to deploy"
   exit 1
 fi
 
-if [ -z "$AWS_DEFAULT_REGION"]
+if [[ -z "$AWS_DEFAULT_REGION"]]
 then
   echo "You must provide AWS_DEFAULT_REGION in order to deploy"
 fi
 
-if [ -n "$1"]
+if [[ -n "$1"]]
 then
   cd "$1"
 fi
